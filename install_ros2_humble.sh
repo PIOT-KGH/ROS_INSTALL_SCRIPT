3b8eb05ecd0e03bf07d54b2af2bef53b93d81d60
PIOT-KGH <122580386+PIOT-KGH@users.noreply.github.com>
2023-02-09 11:40:21 +0900

    Add files via upload

--- install_ros2_humble.sh
+++ install_ros2_humble.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Apache License 2.0
-# Copyright (c) 2021, NTREX CO., LTD.
+# Copyright (c) 2023, PIOT CO., LTD.
 
 echo ""
 echo "[Note] OS version  >>> Ubuntu 22.04 (Jammy Jellyfish)"
@@ -27,7 +27,7 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
 
 echo "[Install ROS 2 packages]"
-sudo apt update && sudo apt install -y ros-$name_ros_version-desktop
+sudo apt update && sudo apt install -y ros-$name_ros_version-desktop ros-$name_ros_version-camera-info-manager ros-$name_ros_version-image-publisher ros-$name_ros_version-magic-enum ros-$name_ros_version-robot-localization libuvc-dev
 
 echo "[Environment setup]"
 source /opt/ros/$name_ros_version/setup.sh
