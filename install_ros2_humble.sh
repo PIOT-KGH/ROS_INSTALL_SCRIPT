daf1e75c2705c830c4d0abb772528dafdb491822
PIOT-KGH <122580386+PIOT-KGH@users.noreply.github.com>
2023-04-18 16:06:01 +0900

    Update install_ros2_humble.sh

--- install_ros2_humble.sh
+++ install_ros2_humble.sh
@@ -27,7 +27,7 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
 
 echo "[Install ROS 2 packages]"
-sudo apt update && sudo apt install -y ros-$name_ros_version-desktop ros-$name_ros_version-camera-info-manager ros-$name_ros_version-image-publisher ros-$name_ros_version-magic-enum ros-$name_ros_version-robot-localization libuvc-dev ros-$name_ros_version-nav2-common ros-$name_ros_version-nav2-util libqt5serialport5-dev libgoogle-glog-dev nlohmann-json3-dev
+sudo apt update && sudo apt install -y ros-$name_ros_version-desktop ros-$name_ros_version-camera-info-manager ros-$name_ros_version-image-publisher ros-$name_ros_version-magic-enum ros-$name_ros_version-robot-localization libuvc-dev ros-$name_ros_version-nav2-* libqt5serialport5-dev libgoogle-glog-dev nlohmann-json3-dev
 
 echo "[Environment setup]"
 source /opt/ros/$name_ros_version/setup.sh
